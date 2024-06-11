9e71b678ea221e70a0e34ddec810f457352ac1af
Eric <ericcui@qq.com>
2024-06-11 21:20:42 +0800

    add sample code

---
+++ SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NewWindowsBtn_Clicked(object sender, EventArgs e)
+        {
+            for (int i = 0; i < int.Parse(PageNumber.Text); i++)
+            {
+                Application.Current?.OpenWindow(new Window(new WebPage()));
+            }
+        }
+    }
+
+}
